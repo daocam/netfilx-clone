a757f7950fb526376366356cb1b6e2e52e0e3e66
daocam <camara.daouda@outlook.fr>
2023-12-04 21:51:25 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="m-5">
+      <Button>Hello from shadcn/ui</Button>
+      <h1>Hello from index page</h1>
+    </div>
+  )
+}
