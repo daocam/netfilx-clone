1f8c255eabcbf734c75600164d94f5c84f64d0d7
daocam <camara.daouda@outlook.fr>
2023-12-09 23:25:14 -0500

    v1

--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -4,6 +4,8 @@ import { Button } from "@/components/ui/button";
 import { Heart, PlayCircle } from "lucide-react";
 import PlayVideoModal from "./PlayVideoModal";
 import { useState } from "react";
+import { addToWatchlist, deleteFromWatchlist } from "../action";
+import { usePathname } from "next/navigation.js";
 
 interface iAppProps {
   title: string;
@@ -29,6 +31,7 @@ export default function MovieCard({
   time
 }: iAppProps) {
   const [open, setOpen] = useState(false);
+  const pathname = usePathname();
 
   return (
     <>
@@ -38,13 +41,17 @@ export default function MovieCard({
 
       <div className="right-5 top-5 absolute z-10">
         {watchList ? (
-          <form>
+          <form action={deleteFromWatchlist}>
+            <input type="hidden" name="watchlistId" value={watchListId} />
+            <input type="hidden" name="pathname" value={pathname} />
             <Button variant="outline" size="icon">
               <Heart className="w-4 h-4 text-red-500" />
             </Button>
           </form>
         ) : (
-          <form>
+          <form action={addToWatchlist}>
+            <input type="hidden" name="movieId" value={movieId} />
+            <input type="hidden" name="pathname" value={pathname} />
             <Button variant="outline" size="icon">
               <Heart className="w-4 h-4" />
             </Button>
--- app/home/[genre]/page.tsx
+++ app/home/[genre]/page.tsx
@@ -1,5 +1,126 @@
-export default function CategoryPage() {
+import MovieCard from "@/app/components/MovieCard";
+import { authOptions } from "@/app/utils/auth";
+import prisma from "@/app/utils/db";
+import { getServerSession } from "next-auth";
+import Image from "next/image.js";
+
+const getData = async (category: string, userId: string) => {
+  switch (category) {
+    case "shows": {
+      const data = await prisma.movie.findMany({
+        where: {
+          category: "show",
+        },
+        select: {
+          id: true, 
+          imageString: true, 
+          title: true, 
+          overview: true,
+          youtubeString: true, 
+          age: true,
+          duration: true,
+          release: true,
+          WatchLists: {
+            where: {
+              userId: userId,
+            }
+          },
+        },
+      });
+      return data;
+    } 
+    case "movies": {
+      const data = await prisma.movie.findMany({
+        where: {
+          category: "movie",
+        },
+        select: {
+          id: true, 
+          imageString: true, 
+          title: true, 
+          overview: true,
+          youtubeString: true, 
+          age: true,
+          duration: true,
+          release: true,
+          WatchLists: {
+            where: {
+              userId: userId,
+            }
+          },
+        },
+      });
+      return data;
+    } 
+    case "recently": {
+      const data = await prisma.movie.findMany({
+        where: {
+          category: "recent",
+        },
+        select: {
+          id: true, 
+          imageString: true, 
+          title: true, 
+          overview: true,
+          youtubeString: true, 
+          age: true,
+          duration: true,
+          release: true,
+          WatchLists: {
+            where: {
+              userId: userId,
+            }
+          },
+        },
+      });
+      return data;
+    } 
+    default: {
+      throw new Error();
+    }
+  }
+}
+
+export default async function CategoryPage({ params }: { params: { genre: string } }) {
+  const session = await getServerSession(authOptions);
+  const data = await getData(params.genre, session?.user?.email as string);
+
   return (
-    <h1>CategoryP</h1>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-5 sm:px-0 mt-10 gap-6">
+      {data.map((movie) => (
+        <div key={movie.id} className="relative h-60">
+          <Image
+            src={movie.imageString}
+            alt={movie.title}
+            width={500}
+            height={400}
+            className="rounded-sm absolute w-full h-full object-cover"
+          />
+          <div className="h-60 relative z-10 w-full transform transition duration-500 hover:scale-125 opacity-0 hover:opacity-100">
+            <div className="bg-gradient-to-b from-transparent via-black/50 to-black z-10 w-full h-full rounded-lg flex items-center justify-center">
+              <Image 
+                src={movie.imageString} 
+                alt={movie.title} 
+                width={800} 
+                height={800} 
+                className="rounded-lg w-full h-full absolute object-cover -z-10" 
+              />
+
+              <MovieCard
+                movieId={movie.id}
+                overview={movie.overview}
+                title={movie.title}
+                watchListId={movie.WatchLists[0]?.id}
+                watchList={movie.WatchLists.length > 0 ? true : false}
+                youtubeUrl={movie.youtubeString}
+                age={movie.age}
+                time={movie.duration}
+                year={movie.release}
+              />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
